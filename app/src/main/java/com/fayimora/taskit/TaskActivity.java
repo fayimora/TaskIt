bc293fe636c36d139d18ccaeaf1c0461c77a303c
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-06-03 10:35:35 +0100

    Use fancy DatePickerDialog for selecting dates

--- app/src/main/java/com/fayimora/taskit/TaskActivity.java
+++ app/src/main/java/com/fayimora/taskit/TaskActivity.java
@@ -1,5 +1,6 @@
 package com.fayimora.taskit;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,49 +8,67 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import java.text.DateFormat;
+import java.util.Calendar;
 
 
 public class TaskActivity extends ActionBarActivity {
 
+    private Task mTask;
+    private Button mDateButton;
+    private Calendar mCal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
 
-        Task task = (Task) getIntent().getSerializableExtra("TaskExtra");
+        mTask = (Task) getIntent().getSerializableExtra("TaskExtra");
 
         EditText taskNameInput = (EditText) findViewById(R.id.task_name);
-        taskNameInput.setText(task.getName());
+        taskNameInput.setText(mTask.getName());
 
-        Button taskDateButton = (Button) findViewById(R.id.task_date);
-        if(task.getDueDate() != null) {
-            DateFormat df = DateFormat.getDateInstance();
-            taskDateButton.setText(df.format(task.getDueDate()));
-        } else
-            taskDateButton.setText("No Date!");
+        mCal = Calendar.getInstance();
+        mCal.setTime(mTask.getDueDate());
+        mDateButton = (Button) findViewById(R.id.task_date);
+        if(mTask.getDueDate() != null)
+            updateButton();
+        else
+            mDateButton.setText("No Date!");
 
         Button saveButton = (Button) findViewById(R.id.save_button);
 
         CheckBox taskDoneInput = (CheckBox) findViewById(R.id.task_done);
-        taskDoneInput.setChecked(task.isDone());
+        taskDoneInput.setChecked(mTask.isDone());
 
-        taskDateButton.setOnClickListener(new View.OnClickListener() {
+        mDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                DatePickerDialog dpd = new DatePickerDialog(TaskActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        mCal.set(year, monthOfYear, dayOfMonth);
+                        updateButton();
+                    }
+                }, mCal.get(Calendar.YEAR), mCal.get(Calendar.MONTH), mCal.get(Calendar.DAY_OF_MONTH));
+                dpd.show();
             }
         });
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                
             }
         });
 
+    }
+
+    private void updateButton(){
+        DateFormat df = DateFormat.getDateInstance();
+        mDateButton.setText(df.format(mTask.getDueDate()));
     }
 
     @Override
